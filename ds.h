DaehwanKimLab/centrifuge

commit 42b7845f218d6df0efb4a6435e3d966904d51e17

Fixing compiler warnings

ds.h
@@ -2781,7 +2781,7 @@ class EBitList {
 
 public:
 
-	explicit EBitList(size_t isz, int cat = 0) : l_(isz, cat) { reset(); }
+	explicit EBitList(size_t isz, int cat) : l_(isz, cat) { reset(); }
 	
 	explicit EBitList(int cat = 0) : l_(cat) { reset(); }
 

bt2_idx.h
@@ -3031,9 +3031,7 @@ public:
 	char *mmFile1_;
 	char *mmFile2_;
     
-    bool                             _compressed; // compressed index?
-    
-	EbwtParams<index_t> _eh;
+    bool _compressed; // compressed index?
 	bool packed_;
     
     EList<pair<string, uint64_t> >   _uid_to_tid; // table that converts uid to tid
@@ -3045,6 +3043,8 @@ public:
     uint64_t _lastGenomeBoundary ;
     uint64_t _boundaryCheckShift ;
     EBitList<128> _boundaryCheck ;
+
+	EbwtParams<index_t> _eh;
 
 	static const uint64_t default_bmax = OFF_MASK;
 	static const uint64_t default_bmaxMultSqrt = OFF_MASK;

commit 114f3d2ffb1e783d27f60e8c64d2e1e5bd3de688

Fix bug introduced by commit 42b7845

ds.h
@@ -2781,9 +2781,9 @@ class EBitList {
 
 public:
 
-	explicit EBitList(size_t isz, int cat) : l_(isz, cat) { reset(); }
+	explicit EBitList(size_t isz, int cat = 0) : l_(isz, cat) { reset(); }
 	
-	explicit EBitList(int cat = 0) : l_(cat) { reset(); }
+	//explicit EBitList(int cat = 0) : l_(cat) { reset(); }
 
 	/**
 	 * Reset to empty state.
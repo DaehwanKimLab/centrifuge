114f3d2ffb1e783d27f60e8c64d2e1e5bd3de688
Florian Breitwieser <florian.bw@gmail.com>
2018-04-03 22:08:38 -0400

    Fix bug introduced by commit 42b7845

--- ds.h
+++ ds.h
@@ -2781,9 +2781,9 @@ class EBitList {
 
 public:
 
-	explicit EBitList(size_t isz, int cat) : l_(isz, cat) { reset(); }
+	explicit EBitList(size_t isz, int cat = 0) : l_(isz, cat) { reset(); }
 	
-	explicit EBitList(int cat = 0) : l_(cat) { reset(); }
+	//explicit EBitList(int cat = 0) : l_(cat) { reset(); }
 
 	/**
 	 * Reset to empty state.
